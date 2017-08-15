21193c07863c6ac095c55840440f47cf2456a054
Chris Lim <chrislim2888@users.noreply.github.com>
2017-08-15 15:23:12 +0800

    update comment

--- example.php
+++ example.php
@@ -4,7 +4,7 @@ require_once 'IP2Location.php';
    Cache whole database into system memory and share among other scripts & websites
    WARNING: Please make sure your system have sufficient RAM to enable this feature
 */
-// $db = new \IP2Location\Database('./databases/IP-COUNTRY-SAMPLE.BIN', \IP2Location\Database::MEMORY_CACHE);
+// $db = new \IP2Location\Database('./databases/IP-COUNTRY-SAMPLE.BIN', \IP2Location\Database::SHARED_MEMORY);
 
 /*
    Cache the database into memory to accelerate lookup speed
